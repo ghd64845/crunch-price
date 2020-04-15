92b5d65b224af1e7e54c77c9521c37728004d89a
LeeJaeHong <fleuve1008@gmail.com>
2020-04-15 17:30:48 +0900

    Updates

---
+++ precache-manifest.c444c97412659bb3f2cdd9a5cb78a756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745bca8fbccbca51bf4f06273a09fe6c",
+    "url": "/crunch-price/index.html"
+  },
+  {
+    "revision": "2dce7fda999df2303ec0",
+    "url": "/crunch-price/static/css/main.7a4a6115.chunk.css"
+  },
+  {
+    "revision": "439b69cad7950c6bdbeb",
+    "url": "/crunch-price/static/js/2.e06dff2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crunch-price/static/js/2.e06dff2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dce7fda999df2303ec0",
+    "url": "/crunch-price/static/js/main.8a27bf3f.chunk.js"
+  },
+  {
+    "revision": "a69e18fb6d2653633302",
+    "url": "/crunch-price/static/js/runtime-main.84430403.js"
+  }
+]);
